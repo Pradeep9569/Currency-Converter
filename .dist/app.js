9e11fe7928296fc8441e8b721f5d97890ab76bc0
Pradeep9569 <pradeeppankaj2004@gmail.com>
2025-10-30 23:27:27 +0530

    files add

---
+++ .dist/app.js
@@ -0,0 +1,89 @@
+const BASE_URL =
+  "https://currency-api.pages.dev/v1/currencies";
+
+  const dropdowns = document.querySelectorAll(".dropdown select");
+  const btn = document.querySelector("form button");
+  const fromCurr = document.querySelector(".from select");
+   const toCurr = document.querySelector(".to select");
+   const msg = document.querySelector(".msg");
+
+  
+  for(let select of dropdowns) {
+    for(currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if(select.name =="from" && currCode==="USD") {
+        newOption.selected="selected";
+    } else if(select.name === "to" && currCode === "INR") {
+        newOption.selected = "selected";
+
+    }
+    select.append(newOption);
+
+  }
+
+  select.addEventListener("change",(evt)=>{
+    updateFlag(evt.target);
+
+        
+    
+  });
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc =`https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+// btn.addEventListener("click", async(evt) => {
+//     evt.preventDefault();
+//     let amount = document.querySelector(".amount input");
+//     let amtVal = amount.value;
+    
+//     if(amtVal ==="" || amtVal <1){
+//         amtVal=1;
+//         amount.value ="1";
+//     }
+
+//    // console.log(fromCurr.value , toCurr.value);
+
+//    const URL =`${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+//    let response = await fetch(URL);
+//    console.log(response);
+    
+
+// });
+
+btn.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  // Fetch only base currency data
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+
+  if (!response.ok) {
+    console.error("Error:", response.status);
+    return;
+  }
+
+  let data = await response.json();
+
+  // Access target currency rate
+  let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+
+  let finalAmount = amtVal * rate;
+ // console.log(`${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`);
+  msg.innerText =`${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
+});
